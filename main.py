9b0d2bd6da4f035b35b1f65b79fafadb47dab41e
Lim Wei Der <limweider@Lims-MacBook-Pro.local>
2024-09-03 17:59:52 +0800

    First commit

---
+++ main.py
@@ -0,0 +1,27 @@
+# starting code to run to initialise the database
+
+from flask_sqlalchemy import SQLAlchemy
+from flask import Flask 
+
+app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+
+db = SQLAlchemy(app) 
+
+class Transaction (db.Model) :
+    txn_id = db.Column(db.Integer, primary_key=True)
+    txn_date = db.Column(db.String, nullable=False)
+    exchange = db.Column(db.String, nullable=False)
+    pic = db.Column(db.String, nullable=False)
+    position = db.Column(db.String, nullable=False)
+    # buy / sell 
+    txn_type = db.Column(db.String, nullable=False)
+    token_amt = db.Column(db.Integer, nullable=False)
+    usd_amt = db.Column(db.Integer, nullable=False)
+
+    # what do you want to show when you print the Transaction instance
+    def __repr__(self) -> str:
+        return f"Transaction ID : {self.txn_id}, txn_date : {self.txn_date}, exchange : {self.exchange}, pic : {self.pic} \
+        , position : {self.position}, txn_type : {self.txn_type}, token_amt : {self.token_amt}, usd_amt : {self.usd_amt}"
+
